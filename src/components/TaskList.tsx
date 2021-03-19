82686b4a9d3dec0bf099687c0be206bb8d62e3d3
Yago Bravim <ybribeiro14@gmail.com>
2021-03-19 17:14:03 -0300

    challenge complete

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,31 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const newTesk: Task = {
+      id: Math.random(),
+      isComplete: false,
+      title: newTaskTitle
+    }
+
+    setTasks(oldState => [...oldState, newTesk])
+    
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTesks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(newTesks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const taskFilter = tasks.filter(task => task.id !== id)
+
+    setTasks(taskFilter);
   }
 
   return (
